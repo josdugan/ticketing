80c0f4fa86a32134d5f02aa1e5360705da43f7d0
josdugan <josdugan@gmail.com>
2021-11-10 21:15:02 -0500

    Add mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -7,6 +9,7 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const PORT = process.env.PORT || 3000;
+const MONGO_DB = process.env.MONGO_DB || 'mongodb://auth-mongo-srv:27017/auth';
 
 const app = express();
 
@@ -23,6 +26,17 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_DB);
+    console.log('Connected to auth db');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
+};
+
+start();
