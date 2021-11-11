ffb27b040cf02841bdd5bcea000a91c329724fb7
josdugan <josdugan@gmail.com>
2021-11-10 20:28:42 -0500

    Add auth routhes, signup, and error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(express.json());
 
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
