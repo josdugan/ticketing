dddd924e2d74318f8c1fee7cbd051bbb0ff9683f
josdugan <josdugan@gmail.com>
2021-11-11 19:35:49 -0500

    Factor out app config into app file

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cookieSession from 'cookie-session';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
