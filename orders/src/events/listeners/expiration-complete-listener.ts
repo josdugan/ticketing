1553c79abc5c46fa761333b142c337825c13ae24
josdugan <josdugan@gmail.com>
2021-12-03 13:13:54 -0500

    Handle expiration complete event

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,45 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  OrderStatus,
+  Subjects,
+} from '@josdugantickets/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { natsWrapper } from '../nats-wrapper';
+import { OrderCancelledPublisher } from '../order-cancelled-publisher';
+import { queueGroupName } from './queue-group-name';
+
+class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(
+    data: ExpirationCompleteEvent['data'],
+    msg: Message
+  ): Promise<void> {
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
+
+export { ExpirationCompleteListener };
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import mongoose from 'mongoose';
 import { app } from './app';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { natsWrapper } from './events/nats-wrapper';
@@ -39,6 +40,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to orders db');
