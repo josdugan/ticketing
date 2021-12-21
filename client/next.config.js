4c05df4739d65df1e9856883e54c3d7c8b82bfd2
josdugan <josdugan@gmail.com>
2021-12-20 20:22:25 -0500

    Add payments and my orders

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    webpackDevMiddleware: config => {
-      config.watchOptions.poll = 300;
-      return config;
-  }
+  webpackDevMiddleware: (config) => {
+    config.watchOptions.poll = 300;
+    return config;
+  },
+  env: {
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  },
 };
--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,18 @@
+import Router from 'next/router';
 import { useEffect, useState } from 'react';
-const OrderShow = ({ order }) => {
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
+
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push('/orders'),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -20,7 +32,18 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey={process.env.stripePublishableKey}
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
